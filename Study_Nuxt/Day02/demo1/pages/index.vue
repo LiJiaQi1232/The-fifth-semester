49d854ba85faaabfc99d8f80e0ea321a72bdc266
LiJiaQi1232 <3607353094@qq.com>
2021-10-06 11:10:08 +0800

    study_nuxt

---
+++ Study_Nuxt/Day02/demo1/pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+div 
+  h1 Hello Nuxters
+  p This page uses less 
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+div{
+  p{
+    color:@secondary
+  }
+}
+</style>
---
+++ Study_Nuxt/Day02/practice/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in list" :key="item.id"><a href="#">{{item.title}}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  // promise
+  // asyncData() {
+  //   return axios.get("https://cnodejs.org/api/v1/topics").then((res) => {
+  //     console.log(res.data.data);
+  //     return{
+  //       list:res.data.data
+  //     }
+  //   });
+  // },
+  // async await
+   async asyncData(){
+     const {data:{data:list}}=await axios.get("https://cnodejs.org/api/v1/topics")
+     return{
+       list
+     }
+   }
+
+};
+</script>
